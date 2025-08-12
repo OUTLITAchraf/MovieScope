a6ac95bc3a937e08ffa9e1b919d3447282b2a81e
OUTLITAchraf <outlitachraf5@gmail.com>
2025-08-12 09:22:53 +0100

    installe app react with vite et installe tailwind et code partie navbar et responsive de navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import NavBar from "./components/NavBar"
+
+function App() {
+
+  return (
+    <>
+    <header className="lg:mx-15 py-3">
+      <NavBar/>
+    </header>
+    <main>
+    </main>
+    <footer></footer>
+    </>
+  )
+}
+
+export default App
